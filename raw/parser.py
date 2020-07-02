04d825040bb58335a4de5d708364054cdb1b821f
Ali Turfah <aturfah@gmail.com>
2020-07-02 09:42:22 -0400

    Calculate max level

--- raw/parser.py
+++ raw/parser.py
@@ -44,6 +44,7 @@ def parse_table(table_node):
     description = None
     force_boost = False
     force_break = False
+    max_level = None
 
     for row in table_node[0]:
         # Check for name
@@ -102,6 +103,7 @@ def parse_table(table_node):
                         'label': node.text,
                         'width': node.attrib.get('style').replace('width:', '')
                     })
+                max_level = len(levels)
             else:
                 for node in row[1:]:
                     levels.append({
@@ -140,7 +142,8 @@ def parse_table(table_node):
         'growth': growth,
         'growth_order': growth_order,
         'force_boost': force_boost,
-        'force_break': force_break
+        'force_break': force_break,
+        'max_level': max_level
     }
 
     # print(output)
